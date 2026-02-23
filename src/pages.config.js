b8a20f350df17b985158aee5f91f95270b448c26
TDC Gaming <1tylerclarke1@gmail.com>
2026-02-23 12:09:28 +0000

    Multi-role system implemented

--- src/pages.config.js
+++ src/pages.config.js
@@ -69,8 +69,6 @@ import PropertyDetails from './pages/PropertyDetails';
 import Search from './pages/Search';
 import Subscription from './pages/Subscription';
 import GuestProfile from './pages/GuestProfile';
-import BecomeHost from './pages/BecomeHost';
-import BecomeCleaner from './pages/BecomeCleaner';
 import __Layout from './Layout.jsx';
 
 
@@ -97,8 +95,6 @@ export const PAGES = {
     "Search": Search,
     "Subscription": Subscription,
     "GuestProfile": GuestProfile,
-    "BecomeHost": BecomeHost,
-    "BecomeCleaner": BecomeCleaner,
 }
 
 export const pagesConfig = {
