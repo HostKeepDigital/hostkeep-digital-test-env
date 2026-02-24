d2473bbe181a444da26f27ee4af0359b4b1899a7
TDC Gaming <1tylerclarke1@gmail.com>
2026-02-24 10:38:20 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { createPageUrl } from "@/utils";
 import { motion } from "framer-motion";
 import { 
