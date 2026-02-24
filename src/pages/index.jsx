60a46e10f51aedd741ebad7597349bb53bf7c21f
TDC Gaming <1tylerclarke1@gmail.com>
2026-02-24 12:13:38 +0000

    Invalid Payment Link Behavior

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-import { createPageUrl } from "@/utils";
-
-export default function Index() {
-  return <Navigate to={createPageUrl("Home")} replace />;
-}
