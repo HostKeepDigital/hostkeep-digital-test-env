881168c206c9b752aced79d5f3b10adb642a5f87
TDC Gaming <1tylerclarke1@gmail.com>
2026-02-26 21:37:12 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -396,7 +396,7 @@ export default function Home() {
                   </Button>
                 </motion.div>
               ))}
-              </div>
+                </div>
               </>
             )}
           </div>
