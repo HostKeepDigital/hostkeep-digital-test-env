072d6dfb70dd3fce8b3552cdfb91c94e80d57356
TDC Gaming <1tylerclarke1@gmail.com>
2026-03-25 08:36:46 +0000

    Manual code change

--- base44/functions/registerFoundingMember/entry.ts
+++ base44/functions/registerFoundingMember/entry.ts
@@ -1,6 +1,6 @@
 import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
 
-const LOGO_URL = 'https://drive.google.com/uc?export=view&id=1yazuu-6sWc7hEOpyTncZpt-P9Cd-UOt1';
+const LOGO_URL = 'https://i.ibb.co/6cwz6PzN/Host-Keep-Digital-Navy-Background.png';
 const FB_ICON = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png';
 const IG_ICON = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/600px-Instagram_icon.png';
 
