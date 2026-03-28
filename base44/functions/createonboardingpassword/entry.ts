d8a4d22b22f7e1ef8f1cb0c7efa990d30cdaf39c
TDC Gaming <1tylerclarke1@gmail.com>
2026-03-28 14:43:18 +0000

    File changes

--- base44/functions/createonboardingpassword/entry.ts
+++ base44/functions/createonboardingpassword/entry.ts
@@ -51,8 +51,11 @@ Deno.serve(async (req) => {
       );
     }
 
-    // 3) Update the user's password and mark email as verified
-    await serviceRole.entities.User.update(user.id, { password, email_verified: true });
+    // 3) Update the user's password via dedicated auth method
+    await serviceRole.auth.changeUserPassword(user.id, password);
+
+    // 4) Mark email as verified on the User entity
+    await serviceRole.entities.User.update(user.id, { email_verified: true });
 
     // 4) Update FoundingMember record
     await serviceRole.entities.FoundingMember.update(member.id, {
