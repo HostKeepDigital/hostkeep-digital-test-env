832ad2fc8129292e20d11b1882cfee71151faed6
TDC Gaming <1tylerclarke1@gmail.com>
2026-03-31 11:01:58 +0000

    Manual code change

--- src/lib/emailTemplate.js
+++ src/lib/emailTemplate.js
@@ -25,7 +25,7 @@ export function buildEmail({ heading, body, buttonText, buttonUrl }) {
           <tr>
             <td style="background-color:#1E3A5F;padding:32px 40px;text-align:center;">
               <img
-                src="https://i.ibb.co/6cwz6PzN/Host-Keep-Digital-Navy-Background.png"
+                src="https://raw.githubusercontent.com/HostKeepDigital/hostkeep-assets/main/HostKeep_Digital_Navy_Background.png"
                 alt="HostKeep Digital"
                 width="200"
                 style="display:block;margin:0 auto;max-width:200px;height:auto;"
