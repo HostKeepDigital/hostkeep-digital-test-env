e5d77ecb00940e7260f3cee347bd78d94d98dea5
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-01 14:50:41 +0000

    Manual code change

--- src/components/ui/select.jsx
+++ src/components/ui/select.jsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { Check, ChevronDown, ChevronUp } from "lucide-react"
-
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { cn } from "@/lib/utils"
 
 const Select = SelectPrimitive.Root
