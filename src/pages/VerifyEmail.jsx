3f5693fdd2a309c8b19c510738d080289c6ac2b8
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-02 21:18:31 +0000

    Manual code change

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,7 +1,7 @@
 import { useSearchParams, useNavigate } from "react-router-dom";
 import EmailVerificationStep from "@/components/founding/EmailVerificationStep";
 
-const CORNWALL_IMG = "https://drive.google.com/uc?export=view&id=1ZmljdO7m9HdHdT_KKSa0S-p2e9ctR5BU";
+const CORNWALL_IMG = "https://raw.githubusercontent.com/HostKeepDigital/hostkeep-assets/main/vecteezy_coastal-path-sign-post-near-bude_6966039.jpg";
 const LOGO_IMG = "https://raw.githubusercontent.com/HostKeepDigital/hostkeep-assets/main/HostKeep_Digital_Navy_Background.png";
 
 export default function VerifyEmail() {
