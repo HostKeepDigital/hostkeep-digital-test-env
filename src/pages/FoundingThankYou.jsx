63cbe9dc0659a5a35f872b6a77c594a289ec5c8c
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-02 23:18:26 +0000

    Manual code change

--- src/pages/FoundingThankYou.jsx
+++ src/pages/FoundingThankYou.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import { CheckCircle } from "lucide-react";
+import { useSearchParams } from "react-router-dom";
+import { CheckCircle, MapPin } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 const CORNWALL_IMG = "https://drive.google.com/uc?export=view&id=1ZmljdO7m9HdHdT_KKSa0S-p2e9ctR5BU";
