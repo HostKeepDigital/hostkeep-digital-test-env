5753cf9ae5028476a8c567bb3de833e1a89aa1c0
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-03 18:30:55 +0000

    Manual code change

--- src/components/channel/ChannelIcon.jsx
+++ src/components/channel/ChannelIcon.jsx
@@ -1,13 +1,18 @@
+const ICONS = {
+  airbnb:
+    "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_Bélo.svg",
+  booking:
+    "https://upload.wikimedia.org/wikipedia/commons/7/7e/Booking.com_logo.svg",
+  vrbo:
+    "https://upload.wikimedia.org/wikipedia/commons/4/4d/VRBO_Logo.svg"
+};
+
 export function ChannelIcon({ channel, className = "" }) {
-  const icons = {
-    airbnb: "https://upload.wikimedia.org/wikipedia/commons/6/69/Airbnb_Logo_Bélo.svg",
-    booking: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Booking.com_logo.svg",
-    vrbo: "https://upload.wikimedia.org/wikipedia/commons/4/4d/VRBO_Logo.svg"
-  };
+  const src = ICONS[channel] || ICONS.airbnb;
 
   return (
     <img
-      src={icons[channel]}
+      src={src}
       alt={channel}
       className={className}
     />
