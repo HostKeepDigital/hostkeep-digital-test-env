e2cc2fb4b8d9962821478c6105c2a856f9f380ff
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-15 12:39:06 +0000

    File changes

--- base44/functions/stripeWebhook/entry.ts
+++ base44/functions/stripeWebhook/entry.ts
@@ -332,5 +332,30 @@ Deno.serve(async (req) => {
     }
   }
 
+  if (event.type === 'account.updated') {
+    const account = event.data.object;
+    if (account.charges_enabled) {
+      try {
+        const users = await base44.asServiceRole.entities.User.filter({ stripe_connect_account_id: account.id });
+        const user = users?.[0];
+        if (user) {
+          // Update User stripe status
+          await base44.asServiceRole.entities.User.update(user.id, { stripe_connect_status: 'verified' });
+
+          // Update FoundingMember stripe_verified flag
+          const members = await base44.asServiceRole.entities.FoundingMember.filter({ user_id: user.id });
+          if (members?.[0]) {
+            await base44.asServiceRole.entities.FoundingMember.update(members[0].id, { stripe_verified: true });
+          }
+
+          // Check all approval gates
+          await base44.asServiceRole.functions.invoke('checkApprovalGates', { user_id: user.id });
+        }
+      } catch (err) {
+        console.error('account.updated handler error:', err);
+      }
+    }
+  }
+
   return Response.json({ received: true });
 });
