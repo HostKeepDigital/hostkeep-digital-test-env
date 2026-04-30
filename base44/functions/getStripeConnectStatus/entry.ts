c3f8b842fb2e3786ce6517bb39d2c44ae3fa14b2
TDC Gaming <1tylerclarke1@gmail.com>
2026-04-30 21:37:36 +0000

    Manual code change

--- base44/functions/getStripeConnectStatus/entry.ts
+++ base44/functions/getStripeConnectStatus/entry.ts
@@ -25,7 +25,10 @@ Deno.serve(async (req) => {
       return Response.json({ status: "not_connected", error: "no_user_id" });
     }
 
-    const user = await serviceRole.entities.User.get(user_id);
+    const sessions2 = await serviceRole.entities.UserSession.filter({ session_token });
+    const email2 = sessions2?.[0]?.email;
+    const users = email2 ? await serviceRole.entities.User.filter({ email: email2 }) : [];
+    const user = users?.[0];
 
     const stripeStatus = user?.stripe_connect_status;
     const hasAccount = !!user?.stripe_connect_account_id;
